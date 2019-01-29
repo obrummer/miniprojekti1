7c182bb218e848bf19afbda6f91cbd0d91b4b555
ansssi89 <ansssi@live.com>
2019-01-29 09:55:33 +0200

    muotoilua1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var fs = require ('fs');
 var express = require('express');
 var router = express.Router();
 
-var peopleArray = [];
+var shoppingArray = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,29 +10,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/lomake', function(req, res, next) {
-  res.render('lomake', {title: "Lomakesivu", nimi: "Lassi Lomake"});
+  res.render('lomake', {title: "Ostoslista"});
 });
 
 router.post('/addperson', function(req, res, next) {
   let newPerson = req.body;
-  newPerson["id"] = peopleArray.length + 1;
-  peopleArray.push(newPerson);
-  let peopleJSON = JSON.stringify(peopleArray);
+  newPerson["id"] = shoppingArray.length + 1;
+  shoppingArray.push(newPerson);
+  let peopleJSON = JSON.stringify(shoppingArray);
   fs.writeFile('./views/files/shopping.json', peopleJSON, function(err) {
     if (err) throw err;
     console.log("shopping.json saved!");
   });
-  res.render('nayta', {title: 'Naytalomakesivu', nimi: 'Lassi Lomake', peopleArray});
+  res.render('nayta', {title: 'Ostoslista', shoppingArray});
 })
 
 fs.readFile('./views/files/shopping.json', function(err, data) {
   console.log("File reading...");
   console.log("data: " + data);
   if (data) {
-  peopleArray = JSON.parse(data);
-  console.log("File read! " + peopleArray);
+  shoppingArray = JSON.parse(data);
+  console.log("File read! " + shoppingArray);
   } else {
-    peopleArray = [];
+    shoppingArray = [];
     console.log("Tyhjä tiedosto");
   }
 });
