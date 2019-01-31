514da54bd8b51c8e53cd7d9c8eb65175888d4828
obrummer <obrummer@gmail.com>
2019-01-31 14:03:36 +0200

    siivousta

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug'); //View Engine löytyy ylläolevasta polusta ja siellä on index.pug.
+app.set('view engine', 'pug'); 
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,9 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//indexRouter käsittelee juureen tulevat pyynnöt käyttän indexRouteria, joka on määritelty edellä.
 app.use('/', indexRouter);
-//Usersiin tulevat pyynnöt käsittelee usersRouter, joka on määritelty edellä.
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
